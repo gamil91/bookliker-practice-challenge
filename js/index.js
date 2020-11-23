c0485c89df9bb39231570e55420167aa9954c5d7
gamil91 <alexis_gamil99@yahoo.com.com>
2020-11-22 17:04:16 -0800

    abstracted a bit

--- js/index.js
+++ js/index.js
@@ -27,8 +27,9 @@ function eachBook(books){
         });
 }
 // get the div to append to
-//for each book, get and create element for the following:
-//image(img), title, subtitle, author(h3), description(p), a (ul) to append to with an id and a list of users (li), a like (button)
+// for each book, get and create element for the following:
+// image(img), title, subtitle, author(h3), description(p)
+// a (ul) to append to with an id and a list of users (li), a like (button)
 
 
 function showBook(book){
@@ -67,48 +68,39 @@ function showBook(book){
     // button has an event listener go to another function
     
     likeBtn = document.createElement("button")
-    likeBtn.addEventListener("click", (e) => likeBook(e, book))
+    likeBtn.addEventListener("click", (e) => bookUsers(e, book))
 
-        for (const user of book.users){
-            if (user.id == 1){
-                likeBtn.textContent = "UNLIKE"
-            }
-            else { likeBtn.textContent = "LIKE"}
+    for (const user of book.users){
+        if (user.id == 1){
+            likeBtn.textContent = "UNLIKE"
         }
+        else { likeBtn.textContent = "LIKE"}
+    }
 
     bookDiv.append(img, title, subtitle, author, description, userList, likeBtn)
     showPanel.appendChild(bookDiv)
 
 }
 
-function likeBook(e, book){
+// if the button's text content is UNLIKE then filter the users for this book without the user id of 1
+//else take the array of the users and add a hash for user id 1
 
-    // if the button's text content is UNLIKE then filter the users for this book without the user id of 1
-
+function bookUsers(e, book){
     if (e.target.textContent == "UNLIKE"){
         let userArr = book.users.filter(user => user.id != 1)
         let formData = {users: userArr}
-
-        configObj = {
-            method: "PATCH",
-            headers: {
-                "Content-Type" : "application/json",
-                "Accept" : "application/json"
-            },
-            body: JSON.stringify(formData)
-        }
-    
-        fetch(`http://localhost:3000/books/${book.id}`, configObj)
-        .then(resp => resp.json())
-        .then(book => showBook(book))
+        likeBook(formData, book)
     }
     else 
     {
-        //else take the array of the users and add a hash for user id 1
-
         let userArr = book.users 
         userArr.push({"id":1, "username":"pouros"})
         let formData = {users: userArr}
+        likeBook(formData, book)
+    }
+}
+
+function likeBook(formData, book){
 
         configObj = {
             method: "PATCH",
@@ -122,12 +114,12 @@ function likeBook(e, book){
         fetch(`http://localhost:3000/books/${book.id}`, configObj)
         .then(resp => resp.json())
         .then(book => showBook(book))
-
     }
     
+    
 
     
 
 
 
-}
+
